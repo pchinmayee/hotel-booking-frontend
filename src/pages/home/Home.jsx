e1ecf7e8c857e1863e7651c906829a084c52bd6c
ashitoshsable <ashitoshsable09@gmail.com>
2022-11-07 23:22:36 +0530

    Explore Around

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Explorearround from './explorearround'
 import Slider from './Slider'
 
 function Home() {
   return <div>
     <Slider />
+    <Explorearround />
   </div>
 }
 
