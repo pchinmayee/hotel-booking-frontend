e8527585ceb682bf02024a525e76c34ffcf831e6
Anish-kumar7641 <72142986+Anish-kumar7641@users.noreply.github.com>
2022-11-08 23:41:23 +0530

    added connect with others

---
+++ src/pages/home/Connect_with_others.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Connect_with_others.css'
+function Connect_with_others() {
+  return (
+    <div>
+        <div className="cwo">Connect With Other Travellers</div>
+      <div id="connect">
+        
+        <div className="card">
+            <div className="img">
+                <img src="https://images.pexels.com/photos/6988608/pexels-photo-6988608.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-6988608.jpg&fm=jpg&_gl=1*1498fw7*_ga*MTc0NzU3MDIzNC4xNjU0Njg4MzEz*_ga_8JE65Q40S6*MTY2NzkxNTc4MS4yLjEuMTY2NzkxNTg0OC4wLjAuMA.." alt=""/></div>
+            <div className="text">Travel Talk</div>
+        </div>
+
+
+        <div className="card">
+            <div className="img">
+                <img src="https://images.pexels.com/photos/13815029/pexels-photo-13815029.jpeg?cs=srgb&dl=pexels-maria-loznevaya-13815029.jpg&fm=jpg" alt=""/></div>
+            <div className="text">India</div>
+        </div>
+
+        <div className="card">
+            <div className="img">
+                <img src="https://images.pexels.com/photos/14310574/pexels-photo-14310574.jpeg?cs=srgb&dl=pexels-bekir-donmez-14310574.jpg&fm=jpg&_gl=1*73bod9*_ga*MTc0NzU3MDIzNC4xNjU0Njg4MzEz*_ga_8JE65Q40S6*MTY2NzkxNTc4MS4yLjEuMTY2NzkxNTg3NS4wLjAuMA.." alt=""/></div>
+            <div className="text">More Communities</div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Connect_with_others
